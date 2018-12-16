765d593f2a94821594346c90a37b00f1fc8075ea
liuyanqun0815 <1163866926@qq.com>
2018-12-16 23:08:28 +0800

    更新vue

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import App from './App.vue';
 
+Vue.use(ElementUI);
+Vue.use(router)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components:{App},
+//   template:'</App>'
+// });
+
+// 引入路由
+// // import router from "./router.js"
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router, // 注入到根实例中
+    render: h => h(App)
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import First from '@/components/First'
+import Home from '@/components/Home'
 import HelloWorld from '@/components/HelloWorld'
+
+import User from '@/components/User'
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  node:'history',
+  routes: [{
+      path: '/user',
+      component: 'User',
+      component: User
+    },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/first',
+      name: 'First',
+      component: First
+    },
+    {
+      paht:'/hello',
+      name:'HelloWorld',
+      components: HelloWorld
     }
   ]
 })
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
