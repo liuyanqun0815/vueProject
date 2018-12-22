da8d635ca272db76d53646b1859158a203dd5c65
liuyanqun0815 <1163866926@qq.com>
2018-12-22 17:40:18 +0800

    commit some bug

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <div class='header'>
-      I am header!
-    </div>
-    <div class='tab'>
-      I am tab
-    </div>
-    <div class='content'>
-      I am content
-    </div>
+    // <div class='header'>
+    //   I am header!
+    // </div>
+    // <div class='tab'>
+    //   I am tab
+    // </div>
+    // <div class='content'>
+    //   I am content
+    // </div>
   </div>
 </template>
 
